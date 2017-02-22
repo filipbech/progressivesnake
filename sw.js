be83e87c638433ac3a9d11ec46d19d129c81c54c
Filip Bech-Larsen <filip@Filips-MacBook-Pro.local>
2017-02-22 10:03:25 +0100

    make pwa

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHENAMES = {
+	CODE: 'v1'
+};
+
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(CACHENAMES.CODE)
+			.then(cache => {
+				return cache.addAll([
+					'/',
+					'/snake.js',
+					'/snake.css'
+				])
+				.then(() => self.skipWaiting());
+			})
+	);
+});	
+
+self.addEventListener('activate',  event => {
+	event.waitUntil(Promise.all([self.clients.claim(),
+		caches.keys().then(keyList => {
+			return Promise.all(keyList.map(function(key) {
+				if (Object.values(CACHENAMES).indexOf(key) == -1) {
+					return caches.delete(key);
+				}
+				return true;
+			}));
+		})
+	]));
+});
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request).then(r => r || fetch(event.request))
+	);
+});
